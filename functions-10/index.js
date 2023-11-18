618d11e670bdd3fee3afbfb47a8e8f0bd694e9e9
Elddar00 <eldaridrizovic00@gmail.com>
2023-11-18 21:23:40 +0100

    something new about functions

--- functions-10/index.js
+++ functions-10/index.js
@@ -107,52 +107,226 @@
 
 // CALL AND APLAY METHODS
 
-const lufthansa = {
-  airline: "Lufthanse",
-  iataCode: "LH",
-  bookings: [],
-  // book: function() {}
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}.`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
+// const lufthansa = {
+//   airline: "Lufthanse",
+//   iataCode: "LH",
+//   bookings: [],
+//   // book: function() {}
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}.`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
 
-lufthansa.book(239, "Eldar Idrizovic");
-lufthansa.book(635, "John Smith");
+// lufthansa.book(239, "Eldar Idrizovic");
+// lufthansa.book(635, "John Smith");
 
-const eurowings = {
-  airline: "Eurowings",
-  iataCode: "EW",
-  bookings: [],
-};
+// const eurowings = {
+//   airline: "Eurowings",
+//   iataCode: "EW",
+//   bookings: [],
+// };
 
-const book = lufthansa.book;
+// const book = lufthansa.book;
 
-//Does NOT work
-// book(23, "Sarah Williams");
+// //Does NOT work
+// // book(23, "Sarah Williams");
 
-//Call method
-book.call(eurowings, 23, "Sarah Williams");
-console.log(eurowings);
+// //Call method
+// book.call(eurowings, 23, "Sarah Williams");
+// console.log(eurowings);
 
-book.call(lufthansa, 239, "Mary Cooper");
-console.log(lufthansa);
+// book.call(lufthansa, 239, "Mary Cooper");
+// console.log(lufthansa);
 
-const swiss = {
-  airline: "Swiss Air Lines",
-  iataCode: "LX",
-  bookings: [],
-};
+// const swiss = {
+//   airline: "Swiss Air Lines",
+//   iataCode: "LX",
+//   bookings: [],
+// };
 
-book.call(swiss, 583, "Mary Cooper");
+// book.call(swiss, 583, "Mary Cooper");
 
 //Apply method
-const flightData = [583, "George Cooper"];
-book.apply(swiss, flightData);
-console.log(swiss);
+// const flightData = [583, "George Cooper"];
+// book.apply(swiss, flightData);
+// console.log(swiss);
 
-//Modern
-book.call(swiss, ...flightData);
+// //Modern
+// book.call(swiss, ...flightData);
+
+// //Blind method
+// // book.call(eurowings, 23, "Sarah Williams");
+
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+
+// bookEW(23, "Steven Williams");
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23("Jonas Schmedtmann");
+// bookEW23("Martha Cooper");
+
+// // With Event Listener
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// // lufthansa.buyPlane();
+
+// document
+//   .querySelector(".buy")
+//   .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// // addVAT = (value) => value + value * 0.23;
+
+// console.log(addVAT(100));
+// console.log(addVAT(23));
+
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// console.log("----------");
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));
+// console.log(addVAT2(23));
+
+// Challenge
+
+// const poll = {
+//   question: "What is your favourite programming languane?",
+//   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     // Get answer
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+//       )
+//     );
+//     console.log(answer);
+
+//     //Register answer
+//     typeof answer === "number" &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
+
+//     this.displayResults();
+//     this.displayResults("string");
+//   },
+//   displayResults(type = "array") {
+//     if (type === "array") {
+//       console.log(this.answers);
+//     } else if (type === "string") {
+//       console.log(`Poll results are ${this.answers.join(", ")}`);
+//     }
+//   },
+// };
+// // poll.registerNewAnswer();
+
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+
+// Immediately invoked function expressions (IIFE)
+
+// const runOnce = function () {
+//   console.log("This will never run again");
+// };
+// runOnce();
+
+// (function () {
+//   console.log("Only one execute", "!!!");
+// })();
+
+// (() => console.log("This will never run again -2"))();
+
+// {
+//   const isPrivate = 23;
+//   var noPrivate = 46;
+// }
+// //Not work
+// // console.log(isPrivate);
+
+// //WORK!
+// console.log(noPrivate);
+
+//CLOSURES
+
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+//Just what learn in last lecture !!!
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f(); // 46
+console.dir(f);
+
+//Re-assigning f function
+h();
+f(); // 1554
+console.dir(f);
+
+//Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
