b89069615c2cd324f0c0a726c65326c064841703
Elddar00 <eldaridrizovic00@gmail.com>
2023-11-23 22:53:18 +0100

    something new

--- Bankist-App/script.js
+++ Bankist-App/script.js
@@ -91,7 +91,6 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -149,3 +148,19 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //     )}`
 // );
 // console.log(movementsDescriptions);
+
+// FILTER
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+
+// // WITHDRAWALS with Arrow function
+// const withdrawals = movements.filter(mov => mov < 0);
+// console.log(withdrawals);
