b66d19627780c7c4399991c6ed26109b635792c5
Elddar00 <eldaridrizovic00@gmail.com>
2023-12-08 19:38:22 +0100

    work with SETTIMEOUT and SETINTERVAL

--- 12-Numbers-Dates-Timer-BankistApp/script.js
+++ 12-Numbers-Dates-Timer-BankistApp/script.js
@@ -276,14 +276,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    //Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      //Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -510,19 +512,43 @@ console.log(new Date(3 * 24 * 60 * 60 * 1000));
 
 // console.log(days1);
 
-const num = 3421414141.23;
+//NUM (Intl)----------------------------------
 
-const options = {
-  style: 'currency',
-  unit: 'celsius',
-  currency: 'EUR',
-  // useGrouping: false,
-};
+// const num = 3421414141.23;
 
-console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
-console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
-console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
-console.log(
-  navigator.language,
-  new Intl.NumberFormat(navigator.language, options).format(num)
+// const options = {
+//   style: 'currency',
+//   unit: 'celsius',
+//   currency: 'EUR',
+/*useGrouping: false,*/
+// };
+
+// console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
+// console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+// console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
+// console.log(
+//   navigator.language,
+//   new Intl.NumberFormat(navigator.language, options).format(num)
+// );
+
+// TIMERS: SETTIMEOUT AND SETINTERVAL
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza ${ing1} and ${ing2}🍕`),
+  3000,
+  ...ingredients
 );
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+//setInterval
+setInterval(function () {
+  const now = new Date();
+  const hour = now.getHours();
+  const minutes = now.getMinutes();
+  const second = now.getSeconds();
+  console.log(`${hour}:${minutes}:${second}`);
+}, 1000);
