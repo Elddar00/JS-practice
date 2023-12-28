0f4ad89eed1fffc223c7bd03e9bd522668160395
Elddar00 <eldaridrizovic00@gmail.com>
2023-12-28 22:40:37 +0100

    script loading: defer and async...

--- 13-Advanced-DOM-Bankist-Website/script.js
+++ 13-Advanced-DOM-Bankist-Website/script.js
@@ -483,16 +483,18 @@ console.log(h1.parentElement.children);
 });
 */
 
-document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('HTML pared and DOM tree built', e);
-});
+// document.addEventListener('DOMContentLoaded', function (e) {
+//   console.log('HTML pared and DOM tree built', e);
+// });
 
-window.addEventListener('load', function (e) {
-  console.log('Page fully loaded', e);
-});
+// window.addEventListener('load', function (e) {
+//   console.log('Page fully loaded', e);
+// });
 
 // window.addEventListener('beforeunload', function (e) {
 //   e.preventDefault();
 //   console.log(e);
 //   e.returnValue = '';
 // });
+
+// Learn about Efficient script loading: Defer and Async
