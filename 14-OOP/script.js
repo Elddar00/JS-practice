93d914b133be7ecec854e460ebce09bd3fd3ca84
Elddar00 <eldaridrizovic00@gmail.com>
2024-01-08 21:36:09 +0100

    Class example...

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -398,6 +398,7 @@ martin.introduce();
 martin.calcAge();
 */
 
+/*
 //Inheritance Between "Classes": Object.create
 
 const PersonProto = {
@@ -427,3 +428,51 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+/*
+// CLASSES EXAMPLE
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-150);
+acc1.deposit(250);
+acc1.withdraw(150);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
+*/
