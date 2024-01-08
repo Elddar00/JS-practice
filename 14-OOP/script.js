537fb4e278c2a723da30b527a456d93b82121d65
Elddar00 <eldaridrizovic00@gmail.com>
2024-01-08 20:57:20 +0100

    inheritance between 'classes': Object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -334,6 +334,7 @@ tesla.brake();
 tesla.accelerate();
 */
 
+/*
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
@@ -395,3 +396,34 @@ class StudentCl extends PersonCl {
 const martin = new StudentCl('Martin Jones', 2000, 'Computer Science');
 martin.introduce();
 martin.calcAge();
+*/
+
+//Inheritance Between "Classes": Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
