4b35dfeb0230bc0b291d60ce4bbaf2c2458b6112
Elddar00 <eldaridrizovic00@gmail.com>
2024-01-23 16:43:15 +0100

    Event Loop In Practice...

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -180,6 +180,8 @@ getCountryData('australia');
 
 */
 
+/*
+
 const whereAmI = function (lat, lng) {
   fetch(
     `https://geocode.xyz/${lat},${lng}?geoit=json&auth=835944364880026506160x71607 `
@@ -206,3 +208,16 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+
+*/
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 10000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
