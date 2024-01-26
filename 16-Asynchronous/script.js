3c43cf8e2b35697e13c9d836e8c10be2fbc71d9e
Elddar00 <eldaridrizovic00@gmail.com>
2024-01-26 18:04:59 +0100

    Other Promise Combinators: RACE. ALLSETTLED And ANY...

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -436,6 +436,7 @@ whereAmI('portugal')
 
   */
 
+/*
 //
 //
 //Running Promise in Parallel
@@ -459,3 +460,58 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('portugal', 'canada', 'tanzania');
+
+*/
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/egypt`),
+    getJSON(`https://restcountries.com/v3.1/name/mexico`),
+  ]);
+
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('request took to long'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v3.1/name/tanzania`),
+  timeout(0.1),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+//
+//
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise any [ES2021]
+
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.reject('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
